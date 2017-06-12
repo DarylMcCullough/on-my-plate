fc418c48e4714186e86c9c1ec6bb85e288cd3ba1
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-06-12 11:19:06 +0000

    get signup signin working, redirect to user page if signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      flash[:notice] = "I see that you're already signed in."
+      redirect_to users_show_path
+    end
+    
   end
 
   def about
