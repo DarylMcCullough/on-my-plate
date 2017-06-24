bffea26f6b88eace5cd1048f22589e33a7ff0506
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-06-24 22:22:29 +0000

    changed order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
+
   resources :items, only: [:create, :destroy]
 
   get 'welcome/index'
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :users, only: [:show]
 
   root 'users#show'
 end
