3f6b7a2f06e1bf34bab19abb537c38af963ffe5b
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-06-25 01:24:46 +0000

    get flash messages working (in footer)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
       if ! current_user
-        flash[:notice] = "You must be signed in to see this page."
+        flash.now[:alert] = "You must be signed in to see this page."
         redirect_to welcome_index_path
       end
       @user = current_user
