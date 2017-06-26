c9a4ff3b7458a88a0e4ac7c7c860d47b7424968b
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-06-26 17:07:31 +0000

    add rake task for deleting expired to do items

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+    def time_til_expiration(dt)
+        if dt < Time.now
+            return "Expired"
+        else
+            distance_of_time_in_words(dt, Time.now)
+        end
+    end
 end
